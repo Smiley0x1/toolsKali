851cd8828eb97ec04ab64993d5c592f0b1d59934
Logan <58321687+Smyli3@users.noreply.github.com>
2022-05-18 22:45:23 -0700

    Update main.py

--- proxy/main.py
+++ proxy/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 import netfilterqueue
+from scapy.all import *
+
+
 
 def process_packet(packet):
-    print(packet)
-    packet.accept()
+    scapy_packet = scapy.IP(packet.get_payload())
+    print(packet.get_payload)
+    packet.accept() #packet.drop # doesn't allow packet to flow
 
 if __name__ == "__main__":
     queue = netfilterqueue.NetfilterQueue()
     queue.bind(0, process_packet)
     queue.run()
+    
